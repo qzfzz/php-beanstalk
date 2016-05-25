6982ff74fe5ae00adc9c9224f22e577f4cb0d21a
qzzz@163.com <qzfzz@163.com>
2016-05-25 11:20:25 +0800

    add windows compile scripts

---
+++ windows_dll/scripts/x64php55ts.bat
@@ -0,0 +1,7 @@
+C:\php-sdk\bin\phpsdk_setvars.bat
+cd C:\php-sdk\php55dev\vc11\x64\php*\
+buildconf
+cscript /nologo configure.js --disable-all --enable-cli --enable-example=shared
+nmake clean
+nmake
+nmake install
---
+++ windows_dll/scripts/x86php54ts.bat
@@ -0,0 +1,9 @@
+cd C:\php-sdk\
+setenv /xp /x86 /release
+C:\php-sdk\bin\phpsdk_setvars.bat
+cd C:\php-sdk\php54dev\vc9\x86\php*\
+buildconf
+cscript /nologo configure.js --disable-all --enable-cli --enable-example=shared
+nmake clean
+nmake
+nmake install
